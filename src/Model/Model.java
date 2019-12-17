8e44d187782bee3fa2886dae54a8af8243047438
Leroy Anozie <leroy.anozie@mailbox.org>
2019-12-17 19:33:36 +0100

    erzeugung von dummy-objekten geloescht

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -23,7 +23,7 @@ public class Model {
     
     private Model()
     {
-    	CreateDummies();
+    	
     }
     
     public static Model getInstance()
@@ -31,29 +31,6 @@ public class Model {
         if(model == null)
             model = new Model();
         return (model);
-    }
-    
-
-    private void CreateDummies() {
-    	LinkedList<FahrradBean> fahrraeder = new LinkedList<FahrradBean>();
-    	fahrraeder.add(erzeugeFahrrad("Gazelle", 26, 300));
-    	fahrraeder.add(erzeugeFahrrad("Gazelle", 28, 150));
-    	fahrraeder.add(erzeugeFahrrad("Gazelle", 26, 199.99));
-    	
-    	for (FahrradBean fahrradBean : fahrraeder) {
-			getFahrraeder().put(fahrradBean.getId(), fahrradBean);
-		}
-    	
-    	
-    }
-    
-    private FahrradBean erzeugeFahrrad(String marke, int groesse, double preis) {
-    	FahrradBean bean = new FahrradBean();
-    	bean.setId(lastFahrradID++);
-    	bean.setGroesse(groesse);
-    	bean.setMarke(marke);
-    	bean.setPreis(preis);
-    	return bean;
     }
     
     
